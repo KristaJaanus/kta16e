d64bc148f7daebf29f05eeabec1e2d8db4672afd
kristajaanus <krista.jaanus@tptlive.ee>
2017-03-14 19:13:02 +0200

    14.03 materjalid

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 
 <meta charset = "utf-8">
 
+<script type="text/javascript">
+    window.onload =function (){
+        (function(){
+        var date = new Date();
+        //paneme aja õigel kujul kokku
+        var time = date.getHours() + ':' 
+        + date.getMinutes()+ ':'
+        + date.getSeconds();
+        //otsime lehelt esimese div tag'i
+        document.getElementsByTagName('div')
+        [0].innerHTML = "Aeg JS'is: " + time;
+        window.setTimeout(arguments.callee,1000);
+        })();
+        
+    }
+        
+        
+</script>
+
 <?php
 require 'components.php';
 require 'function.php';
@@ -19,13 +38,14 @@ home();
 menu($menu_begin, $menu_arr,$menu_end);
 
 //näitame praegust aega ja anname soovitusi
-
 aeg();
+echo "<div></div>";
 // näited teksti kuvamise kohta
 echo "$name esimene PHP's kirjutatud lause!<br>";
 echo '<b>$name esimene PHPs kirjutatud lause!</b><br>';
 echo "Nüüd vaatame täpitähti<br>";
 
 GetVisitorIp();
+counter();
 
 ?>
--- tips.php
+++ tips.php
@@ -3,6 +3,7 @@ require 'function.php';
 
 home();
 // var_dump($_SERVER);
+echo realpath(dirname(__FILE__));
 
 phpinfo();
 
